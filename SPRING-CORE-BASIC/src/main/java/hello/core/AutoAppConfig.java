3fcfc153c112fdf2e5bb8355b8e66fd493693442
gaeunnlee <gaeunnnleee@gmail.com>
2025-08-08 07:02:52 +0900

    test: 컴포넌트 스캔 필터 테스트

--- SPRING-CORE-BASIC/src/main/java/hello/core/AutoAppConfig.java
+++ SPRING-CORE-BASIC/src/main/java/hello/core/AutoAppConfig.java
@@ -1,12 +1,15 @@
 package hello.core;
 
 
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes= Configuration.class)
 )
 public class AutoAppConfig {
