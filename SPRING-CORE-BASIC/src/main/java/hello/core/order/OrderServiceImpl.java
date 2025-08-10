204c05b7b56359e6445059a632776377f4d75fdb
gaeunnlee <gaeunnnleee@gmail.com>
2025-08-11 02:34:49 +0900

    feat: MainDiscountPolicy Annotation 생성

--- SPRING-CORE-BASIC/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SPRING-CORE-BASIC/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -11,11 +12,16 @@ import org.springframework.stereotype.Component;
 
 @Primary
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
