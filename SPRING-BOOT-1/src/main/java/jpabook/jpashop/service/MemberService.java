367260129e9653d849dad05f01e5c7cb2372dc97
gaeunnlee <gaeunnnleee@gmail.com>
2025-09-17 02:35:21 +0900

    feat: 회원 수정 API 구현

--- SPRING-BOOT-1/src/main/java/jpabook/jpashop/service/MemberService.java
+++ SPRING-BOOT-1/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -40,4 +40,10 @@ public class MemberService {
     public Member findById(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
