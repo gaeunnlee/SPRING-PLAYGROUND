823fedc48510d10a6c8fd723dad706dae57cb769
gaeunnlee <gaeunnnleee@gmail.com>
2025-09-19 00:48:48 +0900

    feat: 주문 조회 V4(DTO 바로 조회) 구현

--- SPRING-BOOT-1/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ SPRING-BOOT-1/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -26,6 +26,11 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+                .getResultList();
+    }
     /**
      * JPQL을 통한 검색
      */
@@ -94,8 +99,8 @@ public class OrderRepository {
     public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
                 "select o from Order o" +
-                " join fetch o.member m" +
-                " join fetch o.delivery d", Order.class
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
         ).getResultList();
     }
 }
