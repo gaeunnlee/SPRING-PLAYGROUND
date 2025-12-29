20221d6099590fd1ed2fe51d72feab1725445d7d
gaeunnlee <gaeunnnleee@gmail.com>
2025-12-30 00:21:48 +0900

    chore: Spring Legacy MyBatis 환경 설정

---
+++ SPRING-LEGACY/src/main/java/org/crud_ex/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package org.crud_ex.config;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan(basePackages = {"org.crud_ex.mapper"}, annotationClass = Mapper.class)
+@ComponentScan(basePackages = {"org.crud_ex"})
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer{
+
+    @Override
+    // JSP 경로 (ViewResolver)
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        // prefix: /WEB-INF/view/, suffix: .jsp -> /WEB-INF/views/test.jsp
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+}
