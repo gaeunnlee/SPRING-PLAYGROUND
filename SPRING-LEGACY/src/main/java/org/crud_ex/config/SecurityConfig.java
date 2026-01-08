5713f2b65b0f38fbaf4acea8fb87d9099dddd292
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-08 16:11:47 +0900

    chore: SpringSecurity /favicon.ico 필터 제외

--- SPRING-LEGACY/src/main/java/org/crud_ex/config/SecurityConfig.java
+++ SPRING-LEGACY/src/main/java/org/crud_ex/config/SecurityConfig.java
@@ -2,12 +2,23 @@ package org.crud_ex.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/favicon.ico"
+                );
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
