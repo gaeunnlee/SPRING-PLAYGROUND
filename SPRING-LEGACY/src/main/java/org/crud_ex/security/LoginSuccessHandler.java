0d5ae6676b8df1a7feef13c6b64acbe9369ba69c
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-09 19:10:10 +0900

    feat: 로그인 성공 시 토큰 발급 로직 작성

---
+++ SPRING-LEGACY/src/main/java/org/crud_ex/security/LoginSuccessHandler.java
@@ -0,0 +1,38 @@
+package org.crud_ex.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) throws IOException {
+
+        String email = authentication.getName();
+        String token = jwtProvider.createAccessToken(email);
+
+        Cookie cookie = new Cookie("accessToken", token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); // 운영 HTTPS면 true
+        cookie.setPath("/");
+        cookie.setMaxAge(15 * 60); // 15분 (TTL)
+        response.addCookie(cookie);
+
+        response.sendRedirect(request.getContextPath() + "/members");
+    }
+}
