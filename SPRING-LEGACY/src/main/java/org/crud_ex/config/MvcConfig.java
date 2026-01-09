a3b49d425d3e508acec8d867f406dfe645970012
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-09 11:39:52 +0900

    feat: JWT 설정 파일 작성

--- SPRING-LEGACY/src/main/java/org/crud_ex/config/MvcConfig.java
+++ SPRING-LEGACY/src/main/java/org/crud_ex/config/MvcConfig.java
@@ -7,6 +7,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -20,6 +22,13 @@ import com.zaxxer.hikari.HikariDataSource;
 @ComponentScan(basePackages = {"org.crud_ex"})
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer{
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setLocation(new ClassPathResource("application.properties"));
+        return c;
+    }
 
     @Override
     // JSP 경로 (ViewResolver)
